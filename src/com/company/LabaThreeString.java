0a473f85080d2d675bd2c96664238b81fddf78eb
unknown <armen2002aaa@gmail.com>
2021-12-05 23:04:28 +0300

    New method

--- src/com/company/LabaThreeString.java
+++ src/com/company/LabaThreeString.java
@@ -1,6 +1,6 @@
 package com.company;
 
-import java.util.ArrayList;
+
 import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
@@ -8,7 +8,8 @@ import java.util.regex.Matcher;
 
 public class LabaThreeString {
     public static void main(String[] args) {
-        task_1();
+        //task_1();
+        task_3();
     }
     static void task_1(){
         Scanner sc = new Scanner(System.in);
@@ -32,16 +33,16 @@ public class LabaThreeString {
             for (int i = right; i > left; --i) {
                 if (fulltext[i - 1].length() > fulltext[i].length()) {
                     swap = fulltext[i - 1];
-                    fulltext[i] = swap;
                     fulltext[i - 1] = fulltext[i];
+                    fulltext[i] = swap;
                 }
             }
             ++left;
             for (int i = left; i < right; ++i) {
                 if (fulltext[i].length() > fulltext[i + 1].length()) {
                     swap = fulltext[i];
-                    fulltext[i+1] = swap;
                     fulltext[i] = fulltext[i+1];
+                    fulltext[i+1] = swap;
                 }
             }
             --right;
@@ -50,4 +51,61 @@ public class LabaThreeString {
             System.out.println(fulltext[i] + ": " + fulltext[i].length());
         }
     }
+    static void task_2(){
+        Scanner sc = new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Введите количество строк: ");
+        int n = sc.nextInt();
+        String[] fulltext = new String[n];
+        String swap;
+        for (int i = 0; i < n; i++ ){
+            System.out.printf("\nСтрока №%s\n", (i+1));
+            fulltext[i] = sc1.nextLine();
+        }
+
+        //Сортировка расчёской
+        double factor = 1.247; // Фактор уменьшения
+        int step = n-1;
+
+        while (step >= 1) {
+            for (int i = 0; i + step < n; ++i) {
+                if (fulltext[i].length() >= fulltext[i + 1].length() ) {
+                    swap = fulltext[i+1];
+                    fulltext[i+1] = fulltext[i];
+                    fulltext[i] = swap;
+                }
+            }
+            step /= factor;
+        }
+        for (int i = 0; i < n; i++ ){
+            System.out.println(fulltext[i] + ": " + fulltext[i].length());
+        }
+    }
+    static void task_3(){
+        Scanner sc = new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Введите количество строк: ");
+        int n = sc.nextInt();
+        System.out.println("Введите среднее число строк: ");
+        int middle = sc2.nextInt();
+        String[] fulltext = new String[n];
+        String swap = "";
+        for (int i = 0; i < n; i++ ){
+            System.out.printf("\nСтрока №%s\n", (i+1));
+            fulltext[i] = sc1.nextLine();
+        }
+            for (int i = 0; i < n; ++i) {
+                if (fulltext[i].length() < middle ) {
+                    swap += fulltext[i] + ": " + fulltext[i].length() + "\n";
+                }
+                else {continue;}
+            }
+        System.out.println("Результат: \n"+swap);
+    }
+
+    static void task_4(){
+        
+    }
+
     }
